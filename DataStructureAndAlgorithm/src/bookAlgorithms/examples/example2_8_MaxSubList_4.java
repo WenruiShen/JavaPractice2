cc50dc3a62be686b7d095c26b578fa9ceb4e553e
shenwenrui <shenwenrui@mobike.com>
2018-12-19 01:02:13 +0800

    init

---
+++ DataStructureAndAlgorithm/src/bookAlgorithms/examples/example2_8_MaxSubList_4.java
@@ -0,0 +1,22 @@
+package bookAlgorithms.examples;
+
+
+public class example2_8_MaxSubList_4 implements AlgorithmModel {
+
+    public void run(int[] a){
+
+    }
+
+
+
+    @Override
+    public void excute(){
+
+        int[] arrary = new int[]{1, 2, 3, 4};
+        //Integer[] arrary = new Integer[]{1, 2, 3, 4};
+        //List<Integer> list = new ArrayList<Integer>(Arrays.asList(arrary));
+        run(arrary);
+
+        System.out.println("example2_8_最大子序列_4 excuted!");
+    }
+}
---
+++ DataStructureAndAlgorithm/src/bookAlgorithms/examples/example2_9_BinaryCheck.java
@@ -0,0 +1,22 @@
+package bookAlgorithms.examples;
+
+
+public class example2_9_BinaryCheck implements AlgorithmModel {
+
+    public void run(int[] a){
+
+    }
+
+
+
+    @Override
+    public void excute(){
+
+        int[] arrary = new int[]{1, 2, 3, 4};
+        //Integer[] arrary = new Integer[]{1, 2, 3, 4};
+        //List<Integer> list = new ArrayList<Integer>(Arrays.asList(arrary));
+        run(arrary);
+
+        System.out.println("example2__折半查找 excuted!");
+    }
+}
