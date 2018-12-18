af32af623752d321014cd980a941c17757b65888
shenwenrui <shenwenrui@mobike.com>
2018-12-19 00:05:56 +0800

    init

--- DataStructureAndAlgorithm/src/bookAlgorithms/running.java
+++ DataStructureAndAlgorithm/src/bookAlgorithms/running.java
@@ -1,6 +1,6 @@
-package DataStructureAndAlgorithm;
+package bookAlgorithms;
 
-public class DataStructureAndAlgorithm {
+public class running {
     public static void main(String[] args){
         System.out.println("Hellow World!");
     }
