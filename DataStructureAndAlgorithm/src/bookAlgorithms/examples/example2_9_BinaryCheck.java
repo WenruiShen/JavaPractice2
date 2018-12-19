c0ef213e0729ea123fe4b13ff45072a1179af25c
shenwenrui <shenwenrui@mobike.com>
2018-12-19 23:07:46 +0800

    折半查找。

--- DataStructureAndAlgorithm/src/bookAlgorithms/examples/example2_9_BinaryCheck.java
+++ DataStructureAndAlgorithm/src/bookAlgorithms/examples/example2_9_BinaryCheck.java
@@ -1,20 +1,44 @@
 package bookAlgorithms.examples;
 
-
+/*
+ * Created by shenwenrui on 20181219.
+ * Description: Perform the standard binary search.
+ *              @return index where item is found, or -1 if not found.
+ *              折半查找。
+ */
 public class example2_9_BinaryCheck extends AlgorithmModel {
 
-    public void run(int[] a){
+    public <AnyType extends Comparable<? super AnyType>> int binarySearch(AnyType[] a, AnyType x){
+        int low = 0, high = a.length-1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(a[mid].compareTo(x) < 0){
+                low = mid + 1;
+            }else if (a[mid].compareTo(x)>0){
+                high = mid - 1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
 
+    public int run(Integer[] a, Integer x){
+        return binarySearch(a, x);
     }
 
     @Override
     public void excute(){
-
-        int[] arrary = new int[]{1, 2, 3, 4};
-        //Integer[] arrary = new Integer[]{1, 2, 3, 4};
+        Integer[] arrary = new Integer[]{1, 2, 3, 4, 6, 7, 12, 32, 443, 545, 565, 976};
         //List<Integer> list = new ArrayList<Integer>(Arrays.asList(arrary));
-        run(arrary);
+        Integer target = 443;
+        int index = run(arrary, target);
+        if(index >= 0){
+            System.out.println(String.format("Find target:'%d' at index:'%d'", target, index));
+        }else {
+            System.out.println("Does not find the number!");
+        }
 
-        System.out.println("example2__折半查找 excuted!");
+        System.out.println("example2_9_折半查找 excuted!");
     }
 }
--- DataStructureAndAlgorithm/src/bookAlgorithms/running.java
+++ DataStructureAndAlgorithm/src/bookAlgorithms/running.java
@@ -1,12 +1,12 @@
 package bookAlgorithms;
 
 import bookAlgorithms.examples.AlgorithmModel;
-import bookAlgorithms.examples.example2_8_MaxSubList_4;
+import bookAlgorithms.examples.example2_9_BinaryCheck;
 
 public class running {
     public static void main(String[] args){
         System.out.println("Hellow World!");
-        AlgorithmModel example = new example2_8_MaxSubList_4();
+        AlgorithmModel example = new example2_9_BinaryCheck();
 
         example.excute();
     }
