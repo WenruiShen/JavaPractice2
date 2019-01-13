09329aeb0edf18c7ed000dc042f5f2bb18e9e48d
Thomas Shen <thomas.shen3904@qq.com>
2019-01-13 23:52:56 +0800

    快速排序准备

--- DataStructureAndAlgorithm/src/bookAlgorithms/examples/Sort_Quick.java
+++ DataStructureAndAlgorithm/src/bookAlgorithms/examples/Sort_Quick.java
@@ -4,5 +4,16 @@ package bookAlgorithms.examples;
  * Created by shenwenrui on 20181219.
  * 快排
  */
-public class Sort_Quick {
+public class Sort_Quick extends AlgorithmModel{
+
+    @Override
+    public void excute() {
+        System.out.println("Quick Sort:");
+        int[] arrary = {2, 6, 1, 7, 12, 32, 3233, 44, 37, 4, 457, 443, 545, 565, 976, 3121};
+        System.out.println("arrary:");
+        printIntArray(arrary);
+
+
+
+    }
 }
--- DataStructureAndAlgorithm/src/bookAlgorithms/running.java
+++ DataStructureAndAlgorithm/src/bookAlgorithms/running.java
@@ -1,12 +1,12 @@
 package bookAlgorithms;
 
 import bookAlgorithms.examples.AlgorithmModel;
-import bookAlgorithms.examples.Sort_Merge;
+import bookAlgorithms.examples.Sort_Quick;
 
 public class running {
     public static void main(String[] args){
         System.out.println("Hellow World!");
-        AlgorithmModel example = new Sort_Merge();
+        AlgorithmModel example = new Sort_Quick();
 
         example.excute();
     }
