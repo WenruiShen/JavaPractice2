25786eddb1c5483f24c215f3e0afed4850691d8c
Thomas Shen <thomas.shen3904@qq.com>
2019-01-15 16:57:06 +0800

    直接插入排序:完成

--- DataStructureAndAlgorithm/src/bookAlgorithms/examples/Sort_Insert.java
+++ DataStructureAndAlgorithm/src/bookAlgorithms/examples/Sort_Insert.java
@@ -8,5 +8,29 @@ package bookAlgorithms.examples;
  *  [2].https://blog.csdn.net/u011384489/article/details/78289576#2-%E7%9B%B4%E6%8E%A5%E6%8F%92%E5%85%A5%E6%8E%92%E5%BA%8F-straight-insertion-sort;
  *
  */
-public class Sort_Insert {
+public class Sort_Insert extends AlgorithmModel {
+
+    // 直接插入排序，由小到大
+    public void InsertSortAsc(int[] array){
+        for(int i=1; i<array.length; i++){
+            int temp = array[i];
+            int j= i;
+            for(; j>0 && array[j-1]>temp; j--){ //必须是与temp进行比较
+                array[j]=array[j-1];
+            }
+            array[j]=temp;
+        }
+    }
+
+    @Override
+    public void excute() {
+        System.out.println("Insert Sort:");
+        int[] array = {2, 6, 1, 7, 12, 32, 3233, 32, 44, 37, 44, 32, 32, 457, 32, 443, 32, 545, 32, 565, 32, 976, 3121};
+        System.out.println("arrary:");
+        printIntArray(array);
+
+        InsertSortAsc(array);
+        System.out.println("SortedArrary:");
+        printIntArray(array);
+    }
 }
--- DataStructureAndAlgorithm/src/bookAlgorithms/running.java
+++ DataStructureAndAlgorithm/src/bookAlgorithms/running.java
@@ -1,12 +1,12 @@
 package bookAlgorithms;
 
 import bookAlgorithms.examples.AlgorithmModel;
-import bookAlgorithms.examples.Sort_Quick;
+import bookAlgorithms.examples.Sort_Insert;
 
 public class running {
     public static void main(String[] args){
         System.out.println("Hellow World!");
-        AlgorithmModel example = new Sort_Quick();
+        AlgorithmModel example = new Sort_Insert();
 
         example.excute();
     }
