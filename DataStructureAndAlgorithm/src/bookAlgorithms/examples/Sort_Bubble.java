f5d94242aec543dd9ab92850d58f26263112c61a
Thomas Shen <thomas.shen3904@qq.com>
2019-01-16 16:32:36 +0800

    冒泡排序:准备

---
+++ DataStructureAndAlgorithm/src/bookAlgorithms/examples/Sort_Bubble.java
@@ -0,0 +1,30 @@
+package bookAlgorithms.examples;
+
+/*
+ * Created by shenwenrui on 20190116.
+ * 冒泡排序
+ * 简介:
+ *      1.比较相邻的元素。如果第一个比第二个大，就交换他们两个。
+ *      2.对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。在这一点，最后的元素应该会是最大的数。
+ *      3.针对所有的元素重复以上的步骤，除了最后一个。
+ *      4.持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
+ *
+ * 参考:
+ *      [1].https://blog.csdn.net/u011384489/article/details/78300691;
+ *
+ */
+
+public class Sort_Bubble extends AlgorithmModel {
+
+    @Override
+    public void excute() {
+        System.out.println("Bubble Sort:");
+        int[] array = {2, 6, 1, 7, 12, 32, 3233, 32, 44, 37, 44, 32, 32, 457, 32, 443, 32, 545, 32, 565, 32, 976, 3121};
+        System.out.println("arrary:");
+        printIntArray(array);
+
+        //SelectSortAsc(array, array.length);
+        System.out.println("SortedArrary:");
+        printIntArray(array);
+    }
+}
