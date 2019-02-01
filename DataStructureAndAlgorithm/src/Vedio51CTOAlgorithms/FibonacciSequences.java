e6ddad5bedeca53ea0a80c0b898338bd64905b69
Thomas Shen <thomas.shen3904@qq.com>
2019-02-01 15:52:53 +0800

    偷金子，换硬币：init

--- DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/FibonacciSequences.java
+++ DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/FibonacciSequences.java
@@ -3,6 +3,7 @@ package Vedio51CTOAlgorithms;
 /*
  * Created by shenwenrui on 20190201.
  * Description: 斐波那契数列(Fibonacci Sequences);
+ *              动态规划；
  *              51CTO视频-高频算法面试题:11;
  */
 public class FibonacciSequences {
---
+++ DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/StealGold.java
@@ -0,0 +1,10 @@
+package Vedio51CTOAlgorithms;
+
+/*
+ * Created by shenwenrui on 20190201.
+ * Description: 偷金子;
+ *              动态规划；
+ *              51CTO视频-高频算法面试题:12;
+ */
+public class StealGold {
+}
