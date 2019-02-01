0c504428e731e4c2cc18b5e27ec8976366c16ba9
Thomas Shen <thomas.shen3904@qq.com>
2019-02-01 15:13:33 +0800

    克隆图，安排课程:init

---
+++ DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/CourseArrangement.java
@@ -0,0 +1,10 @@
+package Vedio51CTOAlgorithms;
+
+/*
+ * Created by shenwenrui on 20190201.
+ * Description: 安排课程;
+ *              有向图，环探测;
+ *              51CTO视频-高频算法面试题:9;
+ */
+public class CourseArrangement {
+}
---
+++ DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/GraphClone.java
@@ -0,0 +1,10 @@
+package Vedio51CTOAlgorithms;
+
+/*
+ * Created by shenwenrui on 20190201.
+ * Description: 克隆图;
+ *              有向图克隆;
+ *              51CTO视频-高频算法面试题:8;
+ */
+public class GraphClone {
+}
