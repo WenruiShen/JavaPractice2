9ac5aed60c81995430ac9b548c6785c0a55fe752
Thomas Shen <thomas.shen3904@qq.com>
2019-02-12 14:01:42 +0800

    寻找变化：init

--- DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/SearchChange.java
+++ DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/SearchChange.java
@@ -3,6 +3,10 @@ package Vedio51CTOAlgorithms;
 /*
  * Created by shenwenrui on 20190212.
  * Description: 寻找变化;
+ *              一个数组前段是0，后段是1。
+ *              你能否找到出现的第一个1。
+ *              输入1：nums = {0, 0, 0, 1, 1, 1, 1}, 输出1：3;
+ *              输入2：nums = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1}, 输出2：2;
  *
  *              51CTO视频-高频算法面试题:5;
  */
