cdefa7506077300c1376f06bd9b60c72e5c96c9d
Thomas Shen <thomas.shen3904@qq.com>
2019-02-12 23:47:59 +0800

    寻找变化:完成

--- DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/SearchChange.java
+++ DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/SearchChange.java
@@ -1,5 +1,7 @@
 package Vedio51CTOAlgorithms;
 
+import bookAlgorithms.examples.Model.AlgorithmModel;
+
 /*
  * Created by shenwenrui on 20190212.
  * Description: 寻找变化;
@@ -10,5 +12,51 @@ package Vedio51CTOAlgorithms;
  *
  *              51CTO视频-高频算法面试题:5;
  */
-public class SearchChange {
+public class SearchChange extends AlgorithmModel {
+
+    int search(int nums[], int left, int right){
+        if(left==right){
+            if((left>=nums.length-1 && nums[left]==0) || left==0){
+                return -1;
+            }else{
+                return left;
+            }
+        }
+        int mid = (left+right)/2;
+        if(nums[mid] == 0){
+            left = mid+1;
+        }else {
+            right = mid;
+        }
+        return search(nums, left, right);
+    }
+
+    int searc2(int nums[]){
+        int left = 0, right = nums.length-1;
+        while (left < right){
+            int mid = (left+right)/2;
+            if(nums[mid] == 0){
+                left = mid+1;
+            }else {
+                right = mid;
+            }
+        }
+
+        if((left>=nums.length-1 && nums[left]==0) || left==0){
+            //没有找到
+            return -1;
+        }else{
+            return left;
+        }
+    }
+
+    @Override
+    public void excute() {
+        //int nums[] = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1};
+        int nums[] = {1, 1, 1, 1};
+        printIntArray(nums);
+        //int result = search(nums, 0, nums.length-1);
+        int result = searc2(nums);
+        System.out.println("position:" + result);
+    }
 }
--- DataStructureAndAlgorithm/src/running.java
+++ DataStructureAndAlgorithm/src/running.java
@@ -1,10 +1,10 @@
-import Vedio51CTOAlgorithms.CheckBrackets;
+import Vedio51CTOAlgorithms.SearchChange;
 import bookAlgorithms.examples.Model.AlgorithmModel;
 
 public class running {
     public static void main(String[] args){
         System.out.println("Hellow World!");
-        AlgorithmModel example = new CheckBrackets();
+        AlgorithmModel example = new SearchChange();
 
         example.excute();
     }
