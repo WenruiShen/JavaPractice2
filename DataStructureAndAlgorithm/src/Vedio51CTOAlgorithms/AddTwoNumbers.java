0b8e1ee298193a4a503a70e2e4a9fb51b7ae11f3
shenwenrui <shenwenrui@mobike.com>
2019-02-12 11:01:52 +0800

    两数之和:完成

--- DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/AddTwoNumbers.java
+++ DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/AddTwoNumbers.java
@@ -1,5 +1,10 @@
 package Vedio51CTOAlgorithms;
 
+import bookAlgorithms.examples.Model.AlgorithmModel;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /*
  * Created by shenwenrui on 20190212.
  * Description: 两数之和;
@@ -7,8 +12,37 @@ package Vedio51CTOAlgorithms;
  *              输入1： nums = [4, 5, 7, 10], target=12, 输出1: true;
  *              输入2： nums = [4, 5, 7, 10], target=8,  输出2: false;
  *
+ *              思路：采用逐个hash填充，达到O(n)复杂度。
+ *
  *              51CTO视频-高频算法面试题:2;
  */
-public class AddTwoNumbers {
+public class AddTwoNumbers extends AlgorithmModel {
+    public boolean findTwoNumbers(int[] nums, int target){
+        System.out.println("arrary:");
+        printIntArray(nums);
 
+        boolean result = false;
+        Set<Integer> appeared = new HashSet<Integer>();
+        for(int num : nums){
+            if(appeared.contains(target-num)){
+                return true;
+            }
+            appeared.add(num);
+        }
+        return result;
+    }
+
+    @Override
+    public void excute() {
+        int target =0;
+        boolean result = false;
+
+        target = 12;
+        result = findTwoNumbers(new int[]{4, 5, 7, 10}, target);
+        System.out.println("findTwoNumbers:target:" + target + "\tresult:"+ result);
+
+        target = 8;
+        result = findTwoNumbers(new int[]{4, 5, 7, 10}, target);
+        System.out.println("findTwoNumbers:target:" + target + "\tresult:"+ result);
+    }
 }
--- DataStructureAndAlgorithm/src/running.java
+++ DataStructureAndAlgorithm/src/running.java
@@ -1,12 +1,10 @@
-package bookAlgorithms;
-
+import Vedio51CTOAlgorithms.AddTwoNumbers;
 import bookAlgorithms.examples.Model.AlgorithmModel;
-import bookAlgorithms.examples.DataStructure.BinaryTree;
 
 public class running {
     public static void main(String[] args){
         System.out.println("Hellow World!");
-        AlgorithmModel example = new BinaryTree();
+        AlgorithmModel example = new AddTwoNumbers();
 
         example.excute();
     }
