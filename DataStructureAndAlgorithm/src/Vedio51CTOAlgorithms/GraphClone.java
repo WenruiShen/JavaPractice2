c6dc3454a4c94dfbe98254efd61b6e99d3172f96
Thomas Shen <thomas.shen3904@qq.com>
2019-02-13 12:01:40 +0800

    克隆图:init

--- DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/GraphClone.java
+++ DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/GraphClone.java
@@ -1,10 +1,28 @@
 package Vedio51CTOAlgorithms;
 
+import bookAlgorithms.examples.Model.AlgorithmModel;
+
 /*
  * Created by shenwenrui on 20190201.
  * Description: 克隆图;
+ *              克隆一个无向图，输入：node = {{1, 2}, {0}, {0, 2}}
+ *              解释：
+ *                0
+ *               / \
+ *              1  2
+ *                 /\
+ *                 \/
+ *              输入有三个节点，第0个节点和第1、第2个节点相连，第1个节点和第0个节点相连，第2个节点和第0个、第2个节点相连。
+ *              输出{{1, 2}, {0}, {0, 2}}
+ *              解释：输出的结构相同，但是节点不一样
+ *
  *              有向图克隆;
  *              51CTO视频-高频算法面试题:8;
  */
-public class GraphClone {
+public class GraphClone extends AlgorithmModel {
+
+    @Override
+    public void excute() {
+
+    }
 }
