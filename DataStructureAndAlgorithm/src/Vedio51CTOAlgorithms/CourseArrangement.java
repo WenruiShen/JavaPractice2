a92a003e14e66450b211581b2b4e005974597a1d
Thomas Shen <thomas.shen3904@qq.com>
2019-02-13 18:38:41 +0800

    安排课程：完成

--- DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/CourseArrangement.java
+++ DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/CourseArrangement.java
@@ -2,10 +2,15 @@ package Vedio51CTOAlgorithms;
 
 import bookAlgorithms.examples.Model.AlgorithmModel;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /*
  * Created by shenwenrui on 20190201.
  * Description: 安排课程;
- *              有向图，环探测;
+ *              有向图，环探测(逐个删除出度为0的点);
  *
  *              学校要上课，顺序优先后，能否排排课，把课都上完。
  *              输入：prerequisites = {{1}, {3}, {3}, {}};
@@ -17,9 +22,42 @@ import bookAlgorithms.examples.Model.AlgorithmModel;
  *              51CTO视频-高频算法面试题:9;
  */
 public class CourseArrangement extends AlgorithmModel {
+    public boolean scheduleCourses(int[][] prerequisites){
+        int[] degree = new int[prerequisites.length];
+        List<Integer>[] neighbors = new List[prerequisites.length];
+        Queue<Integer> avilable = new LinkedList<>();
+
+        for(int id =0; id<prerequisites.length; id++){
+            neighbors[id] = new ArrayList<>();
+            degree[id] = prerequisites[id].length;
+            if(degree[id]==0){
+                avilable.offer(id);
+            }
+        }
+
+        for (int from=0; from<prerequisites.length;from++){
+            for (int to: prerequisites[from]){
+                neighbors[to].add(from);
+            }
+        }
+
+        int count=0;
+        while (!avilable.isEmpty()){
+            System.out.println(avilable.peek());
+            for(int id: neighbors[avilable.poll()]){
+                if(--degree[id]==0){
+                    avilable.offer(id);
+                }
+            }
+            count++;
+        }
+        return count==prerequisites.length;
+    }
 
     @Override
     public void excute() {
-
+        int[][] prerequisites = {{1}, {3}, {3}, {}};
+        boolean result = scheduleCourses(prerequisites);
+        System.out.println(result);
     }
 }
--- DataStructureAndAlgorithm/src/running.java
+++ DataStructureAndAlgorithm/src/running.java
@@ -1,10 +1,10 @@
-import Vedio51CTOAlgorithms.GraphClone;
+import Vedio51CTOAlgorithms.CourseArrangement;
 import bookAlgorithms.examples.Model.AlgorithmModel;
 
 public class running {
     public static void main(String[] args){
         System.out.println("Hellow World!");
-        AlgorithmModel example = new GraphClone();
+        AlgorithmModel example = new CourseArrangement();
 
         example.excute();
     }
