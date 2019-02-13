851ee6acb7683a550ee61fe35251cc510fe518e4
Thomas Shen <thomas.shen3904@qq.com>
2019-02-13 14:26:52 +0800

    安排课程：init

--- DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/CourseArrangement.java
+++ DataStructureAndAlgorithm/src/Vedio51CTOAlgorithms/CourseArrangement.java
@@ -1,10 +1,25 @@
 package Vedio51CTOAlgorithms;
 
+import bookAlgorithms.examples.Model.AlgorithmModel;
+
 /*
  * Created by shenwenrui on 20190201.
  * Description: 安排课程;
  *              有向图，环探测;
+ *
+ *              学校要上课，顺序优先后，能否排排课，把课都上完。
+ *              输入：prerequisites = {{1}, {3}, {3}, {}};
+ *              解释：一共四门课，编号分别为0，1，2，3，第0门课需要先完成第1门课，第1、2门课需要先完成第3门课，第3门课没有要求。
+ *              输出：true
+ *
+ *              解释：一个可行的上课顺序是3，2，1，0；
+ *
  *              51CTO视频-高频算法面试题:9;
  */
-public class CourseArrangement {
+public class CourseArrangement extends AlgorithmModel {
+
+    @Override
+    public void excute() {
+
+    }
 }
