e64d1b9be5a3e36497baed2ee9235e7553c5707a
Thomas Shen <thomas.shen3904@qq.com>
2019-02-15 10:05:54 +0800

    双向链表-快排：init

---
+++ DataStructureAndAlgorithm/src/bookAlgorithms/examples/Sort/Sort_Quick_LinkedList.java
@@ -0,0 +1,16 @@
+package bookAlgorithms.examples.Sort;
+
+/*
+ * Created by shenwenrui on 20190215.
+ * 双向链表-快排
+ *
+ * 简介：O(N*logN)，不稳定
+ * 基本思想是：
+ *  1．
+ *
+ * 参考：
+ *  [1].https://www.cnblogs.com/xiaoxuanzhi/p/4445152.html;
+ *
+ */
+public class Sort_Quick_LinkedList {
+}
