7c1cd6917228a23b9cad7586b9cb36bf2bd4ff12
shenwenrui <shenwenrui@mobike.com>
2019-03-15 17:46:42 +0800

    华为：init Huawei_train_20190314_16

---
+++ DataStructureAndAlgorithm/src/exam/Huawei_train_20190314_16.java
@@ -0,0 +1,14 @@
+package exam;
+
+import java.util.Scanner;
+
+/*
+ * 华为：
+ */
+public class Huawei_train_20190314_16 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        input.close();
+    }
+}
